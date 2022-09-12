defbb1454575a540eb11afce5a810a6dbd0d34d3
Delta <davidjoshuasim@gmail.com>
2022-09-11 17:00:26 -0700

    Stable API

--- scraper.js
+++ scraper.js
@@ -3,45 +3,78 @@
 
 const axios = require('axios');
 const cheerio = require('cheerio');
+const express = require('express');
+
+const port = 3000
 const url = "https://www.naeu.playblackdesert.com/en-US/Forum/ForumTopic/Detail?_topicNo=24687&_page="
 
-function getIDSelector(num) {
+function getCountSelector(num) {
     return `#wrap > div > div.container > article > div > div > div.community_body > div.community_detail_wrap > div.detail_area.forum > div > section:nth-child(${num+1}) > div > div.contents_area.editor_area > div.contents_head > div.count`
 }
 
-function getPostSelector(num) {
+function getContentSelector(num) {
     return `#wrap > div > div.container > article > div > div > div.community_body > div.community_detail_wrap > div.detail_area.forum > div > section:nth-child(${num+1}) > div > div.contents_area.editor_area > div.contents_wrap.js-quoteContentsWrap`
 }
 
-function scrapeForum(url, selector) {
+function getURL(topicId, page) {
+    return `https://www.naeu.playblackdesert.com/en-US/Forum/ForumTopic/Detail?_topicNo=${topicId}&_page=${page}`
+}
+
+function scrapeForum(url, countSelector, contentSelector) {
     return axios
         .get(url)
         .then(res => {
             // console.log(`statusCode: ${res.status}`);
             let $ = cheerio.load(res.data);
-            value = $(selector).text();
-            // console.log(value);
-            return value;
+            countText = $(countSelector).text().trim();
+            contentText = $(contentSelector).text().trim();
+            return {count: countText, content: contentText};
         })
         .catch(error => {
             console.error(error);
         });
 }
 
-promises = []
+async function scrapePosts(topicId, maxPages, maxPosts) {
+    console.log("Collecting Data")
+    promises = []
 
-let maxPages = 17
-let maxPosts = 10
-for (let page = 1; page <= maxPages; page++) {
-    for (let post = 1; post <= maxPosts; post++) {
-        promises.push( scrapeForum(url+page, getIDSelector(post)) )
-        promises.push( scrapeForum(url+page, getPostSelector(post)) )
+    // Create a promise for each piece of data we want to pull
+    for (let page = 1; page <= maxPages; page++) {
+        for (let post = 1; post <= maxPosts; post++) {
+            promises.push( scrapeForum( getURL(topicId, page), getCountSelector(post), getContentSelector(post) ) );
+        }
     }
+
+    // Print result of all promises
+    results = await Promise.all(promises)
+        .then( (value) => {
+            // console.log(value);
+            return value;
+        })
+    console.log("Done!");
+    return results
 }
 
-Promise.all(promises)
-        .then( (value) => {
-            console.log(value.toString().trim()+'\r=================')
-        })
-    
 
+// Start Server
+app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// example: http://localhost:3000/api/data/24687
+app.get('/api/data/:topicId', async (req, res) => {
+    // Scrape 1 page and 10 posts
+    // data = await scrapePosts(31, 10);
+    data = await scrapePosts(req.params.topicId, 1, 10);
+    // res.json( data, null, 4 );
+    res.header("Content-Type",'application/json');
+    res.send( JSON.stringify(data, null, 4) );
+});
+
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
