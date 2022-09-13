c6701fbad827c96db1dd1513d3274de91c5f0811
Delta <davidjoshuasim@gmail.com>
2022-09-13 01:41:57 -0700

    Added the ability to use cached page. URL also uses opinion number.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
+const fs = require('fs');
 
 const port = 3000
 const url = "https://www.naeu.playblackdesert.com/en-US/Forum/ForumTopic/Detail?_topicNo=24687&_page="
@@ -12,23 +13,28 @@ function getCountSelector(num) {
     return `#wrap > div > div.container > article > div > div > div.community_body > div.community_detail_wrap > div.detail_area.forum > div > section:nth-child(${num+1}) > div > div.contents_area.editor_area > div.contents_head > div.count`
 }
 
+function getOpinionNo(num) {
+    return `#wrap > div > div.container > article > div > div > div.community_body > div.community_detail_wrap > div.detail_area.forum > div > section:nth-child(${num+1}) > div > div.contents_area.editor_area > div.contents_head > button`
+}
+
 function getContentSelector(num) {
     return `#wrap > div > div.container > article > div > div > div.community_body > div.community_detail_wrap > div.detail_area.forum > div > section:nth-child(${num+1}) > div > div.contents_area.editor_area > div.contents_wrap.js-quoteContentsWrap`
 }
 
-function getURL(topicId, page) {
-    return `https://www.naeu.playblackdesert.com/en-US/Forum/ForumTopic/Detail?_topicNo=${topicId}&_page=${page}`
+function createURL(topicId, opinionNo) {
+    return `https://www.naeu.playblackdesert.com/en-US/Forum/ForumTopic/Detail?_topicNo=${topicId}&_opinionNo=${opinionNo}`
 }
 
-function scrapeForum(url, countSelector, contentSelector) {
+function scrapeForum(topicId, pageNum, countSelector, opinionNoSelector, contentSelector) {
     return axios
-        .get(url)
+        .get(url+pageNum)
         .then( (res) => {
             // console.log(`statusCode: ${res.status}`);
             let $ = cheerio.load(res.data);
             countText = $(countSelector).text().trim();
+            opinionNoText = $(opinionNoSelector).attr('data-opinionno');
             contentText = $(contentSelector).text().trim();
-            return {count: countText, url: url, content: contentText};
+            return {count: countText, url: createURL(topicId, opinionNoText), content: contentText};
         })
         .catch( (error) => {
             console.error(error);
@@ -42,7 +48,7 @@ async function scrapePosts(topicId, maxPages, maxPosts) {
     // Create a promise for each piece of data we want to pull
     for (let page = 1; page <= maxPages; page++) {
         for (let post = 1; post <= maxPosts; post++) {
-            promises.push( scrapeForum( getURL(topicId, page), getCountSelector(post), getContentSelector(post) ) );
+            promises.push( scrapeForum( topicId, page, getCountSelector(post), getOpinionNo(post), getContentSelector(post) ) );
         }
     }
 
@@ -63,42 +69,56 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-// example: http://localhost:3000/api/data/24687
+app.use('/cache', express.static('public'))
+
+// example: /api/data/24687
 app.get('/api/data/:topicId', async (req, res) => {
     // Scrape 1 page and 10 posts
-    // data = await scrapePosts(1, 10);
-    data = await scrapePosts(req.params.topicId, 38, 10);
-    // res.json( data, null, 4 );
+    // data = await scrapePosts(req.params.topicId, 9, 10);
+    data = await scrapePosts(req.params.topicId, 41, 10);
+    jsonData = JSON.stringify(data, null, 4);
     res.header("Content-Type",'application/json');
-    res.send( JSON.stringify(data, null, 4) );
+    res.send( jsonData );
+    fs.writeFile(`public/${req.params.topicId}.json`, jsonData, (err) => {
+        if (err)
+            console.error("err")
+        } );
 });
 
 // Filters out all data with data.content that doesn't contain the filter term
-function filterTerm(json, filterTerms) {
-    // Any post with "class" and "ranger" (case insensitive) with 1 to 30 other characters in between
-    searchRegex = "(class).{1,30}(ranger)"
+function filterClassTerm(json, className) {
+    // Any post with "class" and "<className>" (case insensitive) with 1 to 30 other characters in between
+    searchRegex = `(class).{1,30}(${className})`
     filteredData = [];
     // loop through each data. If one of the filter terms doesn't exists, skip this data and continue
     for (i in json) {
-        // console.log(json[i])
-        for (term in filterTerms) {
-            // if (! json[i].content.toLowerCase().includes(filterTerms[term]) ) {
-            if (! json[i].content.toLowerCase().match(searchRegex) ) {
-                continue;
-            }
-            filteredData.push(json[i])
+        if (! json[i].content.toLowerCase().match(searchRegex) ) {
+            continue;
         }
+        filteredData.push(json[i])
     }
     return filteredData;
 }
 
-app.get('/api/filter/:topicId', (req, res) => {
+// Serves a CACHED version of /api/data with the classname filtered from the list
+// example: /api/cachedFilterClass/24687?class=ranger
+app.get('/api/cachedFilterClass/:topicId', (req, res) => {
+    axios
+        .get(`http://localhost:3000/cache/${req.params.topicId}.json`)
+        .then( (response) => {
+            res.header("Content-Type",'application/json');
+            res.send( JSON.stringify(filterClassTerm(response.data, [req.query.class]), null, 4) );
+        } );
+});
+
+// Serves the LATEST version of /api/data with the classname filtered from the list
+// example: /api/filterClass/24687?class=ranger
+app.get('/api/filterClass/:topicId', (req, res) => {
     axios
         .get("http://localhost:3000/api/data/"+req.params.topicId)
         .then( (response) => {
-            // console.log( filterTerm(res.data, ["ranger"]) )
             res.header("Content-Type",'application/json');
-            res.send( JSON.stringify(filterTerm(response.data, ["ranger"]), null, 4) );
+            res.send( JSON.stringify(filterClassTerm(response.data, [req.query.class]), null, 4) );
         } );
 });
 
